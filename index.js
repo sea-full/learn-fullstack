0f47cceb7e8a8d7375961700d4ca92b483d51165
sea-full <sea_full@yahoo.com>
2023-06-17 15:49:04 +0700

    lupa save

---
+++ index.js
@@ -0,0 +1,43 @@
+// ALGORITMA DAN PSEUDOCODE
+
+/**
+ * Study Case 1
+ * 
+ * Buatlah algoritma dan pseucodeo untuk luas lingkaran.
+ * 
+ * ALGORITMA
+ * 1. Definisikan luas lingkaran: pi * r * r
+ * 2. Buat variable r
+ * 3. Buat variable luas
+ * 4. Isi luas dengan rumus pi kali r kali
+ * 5. Cetak luas lingkaran
+ * 
+ * PSEUDOCODE
+ * STORE r WITH NUMBER
+ * STORE pi WITH 3.14
+ * 
+ * STORE luas WITH pi MUL r MUL r
+ * PRINT luas 
+ * 
+ * Study Case 2
+ * 
+ * Buatlah algoritma dan pseudocode untuk volume tabung.
+ * 
+ * ALGORITMA
+ * 1. Tentukan rumus volumeTabung: pi * r * r * tinggi
+ * 2. Buat variable r
+ * 3. Buat variable tinggi
+ * 4. Buat variable pi
+ * 5. Buat variable volumeTabung
+ * 6. Isi volumeTabung dengan rumus pi * r * r * tinggi
+ * 7. Cetak volumeTabung
+ * 
+ * PSEUDOCODE
+ * STORE r WITH NUMBER
+ * STORE tinggi WITH NUMBER
+ * STORE pi WITH 3.14
+ * 
+ * STORE volumeTabung WITH pi MUL r MUL r MUL tinggi
+ * PRINT volumeTabung
+ * 
+ */
