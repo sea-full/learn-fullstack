5ab6046e2747caa69d2932244286ef582011f7ec
sea-full <sea_full@yahoo.com>
2023-06-18 14:15:35 +0700

    Belajar Algoritma dan Pseudecode

---
+++ latiahan-variable/index.js
@@ -0,0 +1,58 @@
+// Variable dan tipe data
+
+// console.log("Hallo Word");
+
+// Study Case
+// buatlah algoritma dan pseudecode dan codingan dari keliling persegi panjang
+
+
+/** ALGORITMA
+ * 1. Definisikan keliling persegi panjang: 2p + 2l
+ * 2. Buat variable panjang
+ * 3. Buat variable lebar
+ * 3. Buat variable  
+ * 4. Isi keliling dengan rumus 2p + 2l
+ * 5. Cetak luas keliling
+ * 
+ * PSEUDOCODE
+ * STORE panjang WITH NUMBER
+ * STORE lebar WITH NUMBER
+ * 
+ * STORE keliling WITH 2 MUL panjang ADD 2 MUL luas
+ * PRINT keliling 
+*/
+
+
+var panjang = 10;
+var lebar = 2;
+var keliling = 2 * panjang + 2 * lebar;
+
+console.log(keliling);
+
+// Tentukan algoritma dan pseudecode dan codingan dari luas permukaan tabung
+
+/**
+ * ALGORITMA
+ * 1. Definisikan luas permukaan tabung: 2 π r (r + t)
+ * 2. Buat variable pi
+ * 3. Buat variable r
+ * 4. Buat variable  t
+ * 5. Buat variable luas
+ * 6. Isi luas 2 π r (r + t)
+ * 7. Cetak luas permukaan tabung:
+ * 
+ * PSEUDOCODE
+ * STORE pi WITH 3.14
+ * STORE r WITH ANY NUMBER
+ * STORE t WITH ANY NUMBER
+ * 
+ * STORE luas WITH 2 MUL pi MUL r MUL bracket r add t 
+ * PRINT luas 
+ */
+
+var pi = 3.14;
+var r = 5;
+var t = 10;
+var luas = 2 * pi * r * (r + t);
+
+console.log(luas);
